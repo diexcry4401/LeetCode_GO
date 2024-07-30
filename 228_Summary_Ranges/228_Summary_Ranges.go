23e73049f98df6cd3157de9dcedf25b5315ea7e2
Max Tarasov <m1907674@edu.misis.ru>
2024-07-30 12:35:40 +0300

    folder for 1653 and added description for 228

--- 228_Summary_Ranges/228_Summary_Ranges.go
+++ 228_Summary_Ranges/228_Summary_Ranges.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+/*
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b
+
+Example:
+
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9"
+*/
+
 func summaryRanges(nums []int) []string {
 	if len(nums) == 0 {
 		return nil
