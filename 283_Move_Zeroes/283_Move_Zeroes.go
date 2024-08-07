cfb75ce34be94b0510bd22490752a188b4159ea0
granolaa_workPC <m1907674@edu.misis.ru>
2024-08-07 17:11:55 +0300

    283 task done

---
+++ 283_Move_Zeroes/283_Move_Zeroes.go
@@ -0,0 +1,48 @@
+package main
+
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+*/
+
+// func moveZeroes(nums []int) {
+// 	n := len(nums)
+// 	for i := 0; i < n; i++ {
+// 		for j := 0; j < n-i-1; j++ {
+// 			if nums[j] == 0 {
+// 				nums[j], nums[j+1] = nums[j+1], nums[j]
+// 			}
+// 		}
+// 	}
+// 	fmt.Println(nums)
+// }
+
+func moveZeroes(nums []int) {
+	indx := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[indx], nums[i] = nums[i], nums[indx]
+			indx++
+		}
+	}
+}
+
+func main() {
+	// ex1 := []int{0, 1, 0, 3, 12}
+	// ex2 := []int{1, 0, 0}
+	ex3 := []int{0, 0, 1}
+	// moveZeroes(ex1)
+	// moveZeroes(ex2)
+	moveZeroes(ex3)
+
+}
