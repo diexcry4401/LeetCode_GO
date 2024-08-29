6b8850197bf64d152bc6df7b61c2f1384963052a
Max <m1907674@edu.misis.ru>
2024-08-29 11:20:25 +0300

    task 28 with intervals done

--- 28_Find_First_Occurrence_in_a_Str/28_Find_First_Occurrence_in_a_Str.go
+++ 28_Find_First_Occurrence_in_a_Str/28_Find_First_Occurrence_in_a_Str.go
@@ -20,25 +20,11 @@ ans = 2
 
 func strStr(haystack string, needle string) int {
 	ans := -1
-	for i, _ := range haystack {
-		fmt.Printf("haystack[i] = %d, needle[0] = %d, %d, %d\n", haystack[i], needle[0], len(haystack)-1-i, len(needle)-1)
-		if len(haystack)-1-i >= len(needle)-1 {
-			if haystack[i] == needle[0] {
-				count := 0
-				haystackInd := i
-				for ind, _ := range needle {
-					fmt.Println(haystack[haystackInd], haystackInd, needle[ind])
-					if haystack[haystackInd] == needle[ind] {
-						count++
-						haystackInd++
-					} else {
-						fmt.Println("Break")
-						break
-					}
-				}
-				if count == len(needle) {
-					return i
-				}
+	for i := range haystack {
+		if len(haystack)-i >= len(needle) {
+			if haystack[i:i+len(needle)] == needle {
+				fmt.Printf("haystack[%d:%d] = %s, needle = %s\n", i, i+len(needle), haystack[i:i+len(needle)], needle)
+				return i
 			}
 		} else {
 			return ans
